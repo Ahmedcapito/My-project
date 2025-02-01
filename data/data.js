c37a5a47cc817cca13a74e6021fcd8a68c7af94c
Ahmed S Elemam <163877218+Ahmedcapito@users.noreply.github.com>
2025-02-01 20:28:12 +0200

    Api
    Api cloting brand javaScript

---
+++ data/data.js
@@ -0,0 +1,212 @@
+import Star from './star.jpg'
+import Logo from './logo.png'
+import Basket from './basket-preview.png'
+import searchPreview from './searchPreview.png'
+import imageHeader from './Header.jpg'
+// API CARDS 
+import sweatshirt from './Sweatshirt.avif'
+import sweatshirt1 from './Sweatshirt1.avif'
+import sweatshirt2 from './Sweatshirt2.avif'
+import sweatshirt3 from './Sweatshirt3.avif'
+import pullover from './pullover.avif'
+import pullover1 from './pullover.avif'
+import pullover2 from './pullover1.avif'
+import pullover3 from './pullover2.avif'
+import pullover4 from './pullover3.avif'
+import pullover5 from './pullover4.avif'
+import Jacket from './Jacket.avif'
+import Jacket1 from './Jacket1.jpg'
+import Jacket2 from './Jacket3.avif'
+import Jacket3 from './Jacket2.avif'
+import Jacket4 from './Jacket4.avif'
+import Jacket5 from './Jacket5.avif'
+import Jacket6 from './Jacket6.avif'
+import JeansPants from './JeansPants.jpg'
+import JeansPants1 from './JeansPants1.jpg'
+import JeansPants2 from './JeansPants2.avif'
+import JeansPants3 from './JeansPants3.avif'
+import JeansPants4 from './JeansPants4.avif'
+import JeansPants5 from './JeansPants5.avif'
+
+export const dataApi = [
+  {
+    id: "1",
+    name: "Round sweatshirt",
+    price: 750,
+    description: "Round sweatshirt, high quality, cotton, made in Egypt, Turkish material",
+    image: sweatshirt
+  },
+  {
+    id: "2",
+    name: "Round sweatshirt",
+    price: 750,
+    description: "Round sweatshirt, high quality, cotton, made in Egypt, Turkish material",
+    image: sweatshirt1
+  },
+  {
+    id: "3",
+    name: "Round sweatshirt",
+    price: 750,
+    description: "Round sweatshirt, high quality, cotton, made in Egypt, Turkish material",
+    image: sweatshirt2
+  },
+  {
+    id: "4",
+    name: "Round sweatshirt",
+    price: 750,
+    description: "Round sweatshirt, high quality, cotton, made in Egypt, Turkish material",
+    image: sweatshirt3
+  },
+  {
+    id: "5",
+    name: "pullover",
+    price: 650,
+    description: "pullover, high quality, cotton, made in Egypt, Turkish material",
+    image: pullover
+  },
+  {
+    id: "6",
+    name: "pullover",
+    price: 650,
+    description: "pullover, high quality, cotton, made in Egypt, Turkish material",
+    image: pullover1
+  },
+  {
+    id: "7",
+    name: "pullover",
+    price: 650,
+    description: "pullover, high quality, cotton, made in Egypt, Turkish material",
+    image: pullover2
+  },
+  {
+    id: "8",
+    name: "pullover",
+    price: 650,
+    description: "pullover, high quality, cotton, made in Egypt, Turkish material",
+    image: pullover3
+  },
+  {
+    id: "9",
+    name: "pullover",
+    price: 650,
+    description: "pullover, high quality, cotton, made in Egypt, Turkish material",
+    image: pullover4
+  },
+  {
+    id: "10",
+    name: "pullover",
+    price: 650,
+    description: "pullover, high quality, cotton, made in Egypt, Turkish material",
+    image: pullover5
+  },
+  {
+    id: "11",
+    name: "Jacket",
+    price: 650,
+    description: "Jacket, high quality, cotton, made in Egypt, Turkish material",
+    image: Jacket
+  },
+  {
+    id: "12",
+    name: "Jacket",
+    price: 650,
+    description: "Jacket, high quality, cotton, made in Egypt, Turkish material",
+    image: Jacket1
+  },
+  {
+    id: "13",
+    name: "Jacket",
+    price: 650,
+    description: "Jacket, high quality, cotton, made in Egypt, Turkish material",
+    image: Jacket2
+  },
+  {
+    id: "14",
+    name: "Jacket",
+    price: 650,
+    description: "Jacket, high quality, cotton, made in Egypt, Turkish material",
+    image: Jacket3
+  },
+  {
+    id: "15",
+    name: "Jacket",
+    price: 650,
+    description: "Jacket, high quality, cotton, made in Egypt, Turkish material",
+    image: Jacket4
+  },
+  {
+    id: "16",
+    name: "Jacket",
+    price: 650,
+    description: "Jacket, high quality, cotton, made in Egypt, Turkish material",
+    image: Jacket5
+  },
+  {
+    id: "17",
+    name: "Jacket",
+    price: 650,
+    description: "Jacket, high quality, cotton, made in Egypt, Turkish material",
+    image: Jacket6
+  },
+  {
+    id: "18",
+    name: "JeansPants",
+    price: 1900,
+    description: "JeansPants, high quality, cotton, made in Egypt, Turkish material",
+    image: JeansPants
+  },
+  {
+    id: "19",
+    name: "JeansPants",
+    price: 650,
+    description: "JeansPants, high quality, cotton, made in Egypt, Turkish material",
+    image: JeansPants1
+  },
+  {
+    id: "20",
+    name: "JeansPants",
+    price: 650,
+    description: "JeansPants, high quality, cotton, made in Egypt, Turkish material",
+    image: JeansPants2
+  },
+  {
+    id: "21",
+    name: "JeansPants",
+    price: 800,
+    description: "JeansPants, high quality, cotton, made in Egypt, Turkish material",
+    image: JeansPants3
+  },
+  {
+    id: "22",
+    name: "JeansPants",
+    price: 650,
+    description: "JeansPants, high quality, cotton, made in Egypt, Turkish material",
+    image: JeansPants4
+  },
+  {
+    id: "23",
+    name: "JeansPants",
+    price: 650,
+    description: "JeansPants, high quality, cotton, made in Egypt, Turkish material",
+    image: JeansPants5
+  },
+  {
+    id: "24",
+    name: "JeansPants",
+    price: 650,
+    description: "JeansPants, high quality, cotton, made in Egypt, Turkish material",
+    image: JeansPants5
+  },
+];
+
+
+
+
+
+export const assets = {
+  Star,
+  Logo,
+  Basket,
+  searchPreview,
+  imageHeader
+}
